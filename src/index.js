75220f2cfabac4065b2083ed6c9ac0eaad7ce77e
qiurui1231 <209345805@qq.com>
2020-02-10 23:04:00 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import  './style.css'
+// import App from './App';
+
+const { Header, Footer, Content } = Layout;
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header className='header'>Header</Header>
+                <Content className='content'>Content</Content>
+                <Footer>Footer</Footer>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
